4a99869d4caedbef353475ad36ea50106618a3a4
Muhammad Abdullah <mabdullah@conncoll.edu>
2024-11-26 14:12:22 -0500

    Update get_sheet_data.py

--- get_sheet_data.py
+++ get_sheet_data.py
@@ -3,7 +3,6 @@ import requests
 import datetime as dt
 sheet_url = "https://api.sheety.co/7687b746f7aa3562a45758e38efe0c56/flightDeals/sheet1"
 
-
 sheet_response = requests.get(url=sheet_url)
 
 sheet_data = sheet_response.json()["sheet1"]
